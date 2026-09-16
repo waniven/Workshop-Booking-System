c9e05e89e4a7563b637b9fdd333d2c459955f124
waniven <waniven@icloud.com>
2026-09-16 23:50:51 +1200

    added react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import App from "./App";
 import "./index.css";
-import App from "./App.jsx";
+import { ContactForm } from "./components/ContactFromComponent";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/contacts" element={<ContactForm />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
